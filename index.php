37a8ecff2f3bd8c0dd7573375aaec33803789950
mrx06 <0611mrx@gmail.com>
2023-06-30 11:18:39 +0800

    fix

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
             Get Device UDID
           </a>
           <a
-            href="get_mobileconfig.php"
+            href="processes_data.php"
             class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
           >
             Download ESign
